8d5fa1010f7e89112ab360c8c5c320973b683b66
Alexey Zlobin <alexey.zlobin@gmail.com>
2012-03-26 20:47:19 +0400

    some optimisations and clearer interfaces (thanks to @dmedvinsky)

--- inversions/inversions.hs
+++ inversions/inversions.hs
@@ -1,37 +1,36 @@
-module Main (main, sortAndCount, mergeAndCount)
+module Main (main, inversions,
+             sortAndCount, mergeAndCount)
 where
 
 import System.IO
 import Data.List
 
+inversions :: (Ord a) => [a] -> Integer
+inversions = snd . sortAndCount
+
 sortAndCount :: (Ord a) => [a] -> ([a], Integer)
 sortAndCount []  = ([], 0)
 sortAndCount [e] = ([e], 0)
-sortAndCount l   =
-  let ll =  length l
-      splitPos = div ll 2
-      (h1, h2) = splitAt splitPos l
-      (b, x) = sortAndCount h1 
+sortAndCount a   =
+  let splitPos = div (length a) 2
+      (h1, h2) = splitAt splitPos a
+      (b, x) = sortAndCount h1
       (c, y) = sortAndCount h2
       (d, z) = mergeAndCount b c
   in 	(d, x + y + z)
 
 mergeAndCount :: (Ord a) => [a] -> [a] -> ([a], Integer)
-mergeAndCount b c =
-  let (l, x) = mergeAndCountLoop b c ([], 0)
-  in  (reverse l, x)
-
-mergeAndCountLoop :: (Ord a) => [a] -> [a] -> ([a], Integer) -> ([a], Integer)
-mergeAndCountLoop b       []      (acc, x) = ( (reverse b)++acc, x)
-mergeAndCountLoop []      c       (acc, x) = ( (reverse c)++acc, x)
-mergeAndCountLoop (bh:bt) (ch:ct) (acc, x) = 
-  if bh < ch 
-    then mergeAndCountLoop bt (ch:ct) (bh:acc, x)
-    else mergeAndCountLoop (bh:bt) ct (ch:acc, x + (genericLength bt) + 1)
+mergeAndCount b        []       = (b, 0)
+mergeAndCount []       c        = (c, 0)
+mergeAndCount b@(x:xs) c@(y:ys) =
+  if (x < y)
+    then let (list, cnt) = mergeAndCount xs c
+         in (x:list, cnt)
+    else let (list, cnt) = mergeAndCount b  ys
+         in (y:list, cnt + (genericLength b))
 
 main = do
     h <- openFile "IntegerArray.txt" ReadMode
     c <- hGetContents h
     let l = map (read::String->Integer) $ lines c
-    let (_, inv) = sortAndCount l
-    putStrLn $ show inv
+    putStrLn $ show $ inversions l
