c61920195ce59bea912da86b31fac4583ec3f42b
Alexey Zlobin <alexey.zlobin@gmail.com>
2012-04-09 22:20:08 +0400

    min cut

--- min-cut/min-cut.py
+++ min-cut/min-cut.py
@@ -1,27 +1,57 @@
 #!/usr/bin/env python
 
+import random
+
 FILE = 'kargerAdj.txt'
-ROUNDS = 100
+ROUNDS = 1000
 
-def minCut(graph):
-	return 1
+def minCut(edges):
+	while vertexes(edges) > 2:
+		removeOne(edges)
+	return len(edges)
 
-def readGraph():
-	graph = []
+def removeOne(edges):
+	r = random.choice(edges)
+	edges.remove(r)
+	merge(edges, r[0], r[1])
+	removeLoops(edges)
+
+#redirects all adges from b to a
+def merge(edges, a, b):
+	#just make a copy of input for iteration
+	for f,s in list(edges):
+		if f==b:
+			edges.remove((f,s))
+			edges.append((a,s))
+		if s==b:
+			edges.remove((f,s))
+			edges.append((f,a))
+
+def removeLoops(edges):
+	for f,s in list(edges):
+		if f==s:
+			edges.remove((f,s))
+
+
+def vertexes(edges):
+	v = set()
+	for f,s in edges:
+		v.add(f)
+		v.add(s)
+	return len(v)
+
+def readEdges():
+	edges = []
 	with open(FILE, 'r') as f:
 		for l in f.readlines():
 			nodeLine = map(int, l.split())
-			graph.insert(nodeLine[0], [(x, 1) for x in nodeLine[1:]])
-	return graph
-
-def copy(graph):
-	new = []
-	for v in graph:
-		new.append(list(v))
-	return new
-
+			fst = nodeLine[0]
+			for snd in nodeLine[1:]:
+				if (snd, fst) not in edges:
+					edges.append((fst, snd))
+	return edges
 
 if __name__ == '__main__':
-	g = readGraph()
-	r = (minCut(copy(g)) for i in range(0, ROUNDS))
+	e = readEdges()
+	r = (minCut(list(e)) for i in range(0, ROUNDS))
 	print min(r)
