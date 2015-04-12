503e211276b626b6fef8c5f8b2a982cd8716e316
Alexey Zlobin <alexey.zlobin@gmail.com>
2015-04-12 23:33:15 +0400

    zbs

--- clustering/cluster_big.py
+++ clustering/cluster_big.py
@@ -12,7 +12,10 @@ class Node(object):
 		self.size = 1
 
 	def __repr__(self):
-		return 'Node(%s)'%(self.bits)
+		return repr(self.bits)
+
+	def __hash__(self):
+		return hash(self.bits)
 
 def find(p):
 	if (p.parent is p):
@@ -32,35 +35,54 @@ def union(p1, p2):
 	r2.parent = r1
 	return True
 
-def readEdges():
-	nodes = []
+def readNodes():
+	nodes = {}
 	with open(FILE, 'r') as f:
 		lvs = map(int, f.readline().split())
 		nodesCnt = lvs[0]
 		bitsCnt = lvs[1]
 		for l in f.readlines():
-			lvs = map(int, l.split())
+			lvs = l.split()
 			if len(lvs) != bitsCnt:
 				raise Exception('Wrong number of bits')
-			bits = tuple(lvs)
-			nodes.append(Node(bits))
+			bits = tuple([s=='1' for s in lvs])
+			node = Node(bits)
+			nodes[bits]=node
 	return (nodesCnt, nodes)
 
-def adges(nodes):
-	for a in nodes:
-		for b in nodes:
-			if a is not b:
-				yield (a, b)
+def gen(bits, dist):
+	l = len(bits)
+	if dist==1:
+		for i in xrange(0, l):
+			yield bits[:i]+(not bits[i],)+bits[i+1:]
+	elif dist==2:
+		for i in xrange(0, l):
+			for j in xrange(i+1, l):
+				yield bits[:i]+(not bits[i],)+bits[i+1:j]+(not bits[j],)+bits[j+1:]
+	else:
+		raise Exception()
 
-def distance(a, b):
-	d = 0
-	for i in xrange(0, len(a.bits)):
-		if a.bits[i] != b.bits[i]:
-			d+=1
-	return d
+def merge(nodes, dist):
+	for n in nodes.itervalues():
+		for cb in gen(n.bits, dist):
+			if cb in nodes:
+				union(n, nodes[cb])
 
 if __name__ == '__main__':
-	(numNodes, nodes) = readEdges()
-	print(len(nodes))
-	l = sum( (1 for x in adges(nodes) if distance(x[0], x[1])==1) )
-	print(l)
+	(cnt, nodes) = readNodes()
+	# c = 0
+	# for b in nodes.itervalues():
+	# 	c+=1
+	# 	print(">>> "+str(b))
+	# 	for g in gen(b.bits, 1):
+	# 		print(g)
+	# 	if c == 10:
+	# 		break;
+
+	merge(nodes, 1)
+	merge(nodes, 2)
+	
+	tmp = set()
+	for n in nodes.itervalues():
+		tmp.add(find(n))
+	print(len(tmp))
