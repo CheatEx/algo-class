4706eb360bf672594761780282de7c943f522ee5
Alexey Zlobin <alexey.zlobin@jetbrains.com>
2015-05-07 13:46:11 +0300

    WIP

--- jobs/jobs.py
+++ jobs/jobs.py
@@ -5,52 +5,66 @@ import sys
 FILE = 'jobs.txt'
 
 class Job(object):
-	def __init__(self, weight, length):
-		super(Job, self).__init__()
-		self.weight = weight
-		self.length = length
+  def __init__(self, weight, length):
+    super(Job, self).__init__()
+    self.weight = weight
+    self.length = length
 
-	def __repr__(self):
-		return "[w=%s, l=%s]"%(self.weight, self.length)
+  def __repr__(self):
+    return "[w=%s, l=%s]"%(self.weight, self.length)
 
 def readJobs():
-	jobs = list()
-	with open(FILE, 'r') as f:
-		num = f.readline()
-		for l in f.readlines():
-			split = l.split()
-			jobs.append(Job(int(split[0]), int(split[1])))
-	return jobs
+  jobs = list()
+  with open(FILE, 'r') as f:
+    num = f.readline()
+    for l in f.readlines():
+      split = l.split()
+      jobs.append(Job(int(split[0]), int(split[1])))
+  return jobs
 
 def weighedSum(jobs):
-	res = 0
-	t = 0
-	for j in jobs:
-		t += j.length
-		res += t*j.weight
-	return res
+  res = 0
+  t = 0
+  for j in jobs:
+    t += j.length
+    res += t*j.weight
+ #   print('Ratio=%d, completion=%s, total=%s'%(j.weight/j.length, t, res))
+  return res
 
 def diffCmp(j1, j2):
-	j1d = j1.weight - j1.length
-	j2d = j2.weight - j2.length
-	if j1d<j2d:
-		return -1
-	elif j1d>j2d:
-		return 1
-	else:
-		if j1.weight < j2.weight:
-			return -1
-		elif j1.weight > j2.weight:
-			return 1
-		else:
-			return 0
+  j1d = j1.weight - j1.length
+  j2d = j2.weight - j2.length
+  if j1d<j2d:
+    return -1
+  elif j1d>j2d:
+    return 1
+  else:
+    if j1.weight < j2.weight:
+      return -1
+    elif j1.weight > j2.weight:
+      return 1
+    else:
+      return 0
 
+def ratioCmp(j1, j2):
+  j1r = float(j1.weight)/j1.length
+  j2r = float(j2.weight)/j2.length
+  if j1r > j2r:
+    return 1
+  elif j1r < j2r:
+    return -1
+  else:
+    return 0
 
 if __name__ == '__main__':
-	jobs = readJobs()
-	byDiff = sorted(jobs, reverse = True,
-		cmp = diffCmp)
-	print(weighedSum(byDiff))
-	byRatio = sorted(jobs, reverse=True,
-		key = lambda j: j.weight / j.length)
-	print(weighedSum(byRatio))
+  jobs = readJobs()
+  byDiff = sorted(jobs, reverse=True,
+    cmp = diffCmp)
+  print(">>> By difference")
+  print(weighedSum(byDiff))
+  byRatio = sorted(jobs, reverse=True,
+    cmp = ratioCmp)
+  print(">>> By ratio")
+  #print(len(jobs))
+  #print(len(byRatio))
+  print(weighedSum(byRatio))
--- tsp/tsp.py
+++ tsp/tsp.py
@@ -3,6 +3,8 @@
 import sys
 import itertools
 import math
+#https://code.google.com/p/yappi/wiki/usageyappi_v092
+#import yappi
 
 def readProblem(fileName):
 	points = []
@@ -21,25 +23,30 @@ def calc(points):
 	np = len(points)
 	prev = {}
 	cur = {}
-	for m in xrange(2, np):
+	initS = frozenset([1])
+	prev[initS] = array(np, initS)
+	for m in xrange(2, np+1):
 		print("m=%s"%(m))
-		(prev, cur) = (cur, prev)
 		cur.clear()
 		for s in genSet(np, m):
-			l = [-sys.maxint]*np
-			l[0] = 0 if len(s) == 1 and 1 in s else sys.maxint
+			l = array(np, s)
 			cur[s] = l
 			for j in s:
 				if j != 1:
 					l[j-1] = min(prev, s, j, points)
+		(prev, cur) = (cur, prev)
 
 	m = sys.maxint
 	for j in xrange(2, np+1):
-		d = cur[frozenset(xrange(1, np+1))] + dist(points, j, 1)
+		d = prev[frozenset(xrange(1, np+1))] + dist(points, j, 1)
 		if d < m:
 			m = d
 	return m
 
+def array(np, s):
+	l = [-sys.maxint]*np
+	l[0] = 0 if len(s) == 1 and 1 in s else sys.maxint
+	return l
 					
 def min(A, s, j, points):
 	m = sys.maxint
@@ -51,9 +58,11 @@ def min(A, s, j, points):
 	return m
 
 def dist(points, j, k):
-	dx = points[j-1][1] - points[k-1][1]
-	dy = points[j-1][2] - points[k-1][2]
-	math.sqrt(dx*dx + dy*dy)
+	a = points[j-1]
+	b = points[k-1]
+	dx = a[1] - b[1]
+	dy = a[2] - b[2]
+	return math.sqrt(dx*dx + dy*dy)
 
 def genSet(max, size):
 	s = range(1, max+1)
@@ -64,5 +73,7 @@ def genSet(max, size):
 
 if __name__ == '__main__':
 	points = readProblem('tsp.txt')
+	#yappi.start()
 	m = calc(points)
+	#yappi.get_func_stats().print_all()
 	print('TSP %s'%(m))
