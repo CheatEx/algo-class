60d4423727b846333be105ca55fd0e3de6b1d2fa
Alexey Zlobin <alexey.zlobin@jetbrains.com>
2015-05-07 14:00:42 +0300

    WIP-2

--- tsp/tsp.py
+++ tsp/tsp.py
@@ -21,11 +21,12 @@ def readProblem(fileName):
 
 def calc(points):
 	np = len(points)
+	cache = [frozenset([x]) for x in xrange(1, np+1)]
 	prev = {}
 	cur = {}
 	initS = frozenset([1])
 	prev[initS] = array(np, initS)
-	for m in xrange(2, np+1):
+	for m in xrange(2, 8):
 		print("m=%s"%(m))
 		cur.clear()
 		for s in genSet(np, m):
@@ -33,7 +34,7 @@ def calc(points):
 			cur[s] = l
 			for j in s:
 				if j != 1:
-					l[j-1] = min(prev, s, j, points)
+					l[j-1] = min(prev, s, j, points, cache)
 		(prev, cur) = (cur, prev)
 
 	m = sys.maxint
@@ -48,11 +49,11 @@ def array(np, s):
 	l[0] = 0 if len(s) == 1 and 1 in s else sys.maxint
 	return l
 					
-def min(A, s, j, points):
+def min(A, s, j, points, cache):
 	m = sys.maxint
 	for k in s:
 		if k != j:
-			d = A[s - frozenset([j])][k-1] + dist(points, j, 1)
+			d = A[s - cache[j-1]][k-1] + dist(points, k, j)
 			if d < m:
 				m = d
 	return m
@@ -77,3 +78,4 @@ if __name__ == '__main__':
 	m = calc(points)
 	#yappi.get_func_stats().print_all()
 	print('TSP %s'%(m))
+
