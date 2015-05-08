2f428fadd2430f5365d5a69e4b2a54d325412331
Alexey Zlobin <alexey.zlobin@jetbrains.com>
2015-05-08 11:20:01 +0300

    better opt

--- tsp/tsp.py
+++ tsp/tsp.py
@@ -21,12 +21,13 @@ def readProblem(fileName):
 
 def calc(points):
 	np = len(points)
-	cache = [frozenset([x]) for x in xrange(1, np+1)]
+	sc = [frozenset([x]) for x in xrange(1, np+1)]
+	dists = buildDists(points)
 	prev = {}
 	cur = {}
 	initS = frozenset([1])
 	prev[initS] = array(np, initS)
-	for m in xrange(2, 8):
+	for m in xrange(2, np+1):
 		print("m=%s"%(m))
 		cur.clear()
 		for s in genSet(np, m):
@@ -34,12 +35,12 @@ def calc(points):
 			cur[s] = l
 			for j in s:
 				if j != 1:
-					l[j-1] = min(prev, s, j, points, cache)
+					l[j-1] = min(prev, s, j, dists, np, sc)
 		(prev, cur) = (cur, prev)
 
 	m = sys.maxint
 	for j in xrange(2, np+1):
-		d = prev[frozenset(xrange(1, np+1))] + dist(points, j, 1)
+		d = prev[frozenset(xrange(1, np+1))] + dists[j-1]
 		if d < m:
 			m = d
 	return m
@@ -49,11 +50,12 @@ def array(np, s):
 	l[0] = 0 if len(s) == 1 and 1 in s else sys.maxint
 	return l
 					
-def min(A, s, j, points, cache):
+def min(A, s, j, dists, np, cache):
+	old = A[s - cache[j-1]]
 	m = sys.maxint
 	for k in s:
 		if k != j:
-			d = A[s - cache[j-1]][k-1] + dist(points, k, j)
+			d = old[k-1] + dists[(j-1)*np+(k-1)]
 			if d < m:
 				m = d
 	return m
@@ -64,6 +66,9 @@ def dist(points, j, k):
 	dx = a[1] - b[1]
 	dy = a[2] - b[2]
 	return math.sqrt(dx*dx + dy*dy)
+
+def buildDists(points):
+	return [dist(points, x, y) for x in xrange(1, len(points)+1) for y in xrange(1, len(points)+1)]
 
 def genSet(max, size):
 	s = range(1, max+1)
